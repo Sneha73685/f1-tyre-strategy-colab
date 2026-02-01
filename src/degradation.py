c19bb49f171d5c685383990178a3593fba14d34c
Sneha <99313227+Sneha73685@users.noreply.github.com>
2026-02-01 15:37:25 +0530

    new edits

--- src/degradation.py
+++ src/degradation.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 class TyreDegradationModel:
-    def __init__(self, alpha=0.02):
+    def __init__(self, alpha=0.03):
         self.alpha = alpha
 
     def fit(self, data):
+        self.laps = data["lap"].values
         return self
 
-    def predict(self, stint_length):
-        laps = np.arange(stint_length)
-        return np.exp(-self.alpha * laps)
+    def predict(self):
+        return np.exp(-self.alpha * self.laps)

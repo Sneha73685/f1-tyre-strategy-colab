8d5d4167d4e43b560ac1ff9093dca155e5bea9c6
Sneha <99313227+Sneha73685@users.noreply.github.com>
2026-02-01 15:39:29 +0530

    new file edits

--- src/strategy.py
+++ src/strategy.py
@@ -2,9 +2,12 @@ import numpy as np
 
 def simulate_strategy(degradation_model, total_laps, pit_window):
     strategy_cost = []
+
     for pit_lap in range(pit_window[0], pit_window[1]):
-        stint1 = degradation_model.predict(pit_lap)
-        stint2 = degradation_model.predict(total_laps - pit_lap)
+        stint1 = degradation_model.predict()[:pit_lap]
+        stint2 = degradation_model.predict()[pit_lap:total_laps]
+
         cost = np.sum(stint1) + np.sum(stint2)
         strategy_cost.append((pit_lap, cost))
+
     return strategy_cost

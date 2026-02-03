c055d7b696c44aae02f05984fc0551ae6894a5d0
Sneha <99313227+Sneha73685@users.noreply.github.com>
2026-02-03 14:19:35 +0530

    layer 2 initialised

---
+++ src/driver_profiles.py
@@ -0,0 +1,5 @@
+DRIVER_STYLE_FACTORS = {
+    "Aggressive": 1.1,
+    "Balanced": 1.0,
+    "TyreSaver": 0.9
+}
---
+++ src/track_profiles.py
@@ -0,0 +1,8 @@
+TRACK_DEGRADATION_FACTORS = {
+    "Monaco": 0.7,
+    "Singapore": 1.1,
+    "Hungary": 1.3,
+    "Silverstone": 1.0,
+    "Suzuka": 1.2,
+    "Monza": 0.8
+}
